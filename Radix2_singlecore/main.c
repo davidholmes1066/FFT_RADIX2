b368bedd08cb82b49dba078774fe4d24baf8b597
David Holmes <david.holmes@kpnmail.nl>
2023-01-21 10:18:04 +0000

    added comparison fft and avr_fft in main.c
    added test that compares the avr_fft to the fft functions using complex.h

--- Radix2_singlecore/main.c
+++ Radix2_singlecore/main.c
@@ -14,24 +14,27 @@ int main(void)
     Lookup_Reverse = init_BRLookup();
     Window = init_Window();
 
-    complexfloat *E;
-    complex float T;
-
-    E = init_avr_Wlookup();
-
-    for (uint16_t i = 0; i < (N/2); i++)
-    {
-        printf("imag: %f\treal: %f\t\t", E[i].im, E[i].re);
-        printf("imag: %f\t, real: %f\n",cimagf(W[i]),crealf(W[i]));
-    }
+    complexfloat *FFT_avr_Array = init_avr_fft();
+    complexfloat *W_avr = init_avr_Wlookup();
 
     for(int i = 0; i < N; i++)
     {
         FFT_Array[Lookup_Reverse[i]] = 1+1*I;                           //Test input
+
+        FFT_avr_Array[Lookup_Reverse[i]].re = 1;                        //Test input avr complex
+        FFT_avr_Array[Lookup_Reverse[i]].im = 1;
     }
 
     apply_Window(FFT_Array, Window, Lookup_Reverse);                    //Apply window function
     calc_FFT(FFT_Array, W);                                             //Calculate FFT
+
+    apply_avr_Window(FFT_avr_Array,Window,Lookup_Reverse);     //AVR apply window function
+    calc_avr_FFT(FFT_avr_Array,W_avr);                      //AVR apply FFT
+
+    for(int i = 0; i < N; i++)
+    {
+        printf("AVRI: %f\tAVRR: %f\t\tI: %f\tR: %f\n",FFT_avr_Array[i].im, FFT_avr_Array[i].re,cimagf(FFT_Array[i]),crealf(FFT_Array[i]));
+    }
 
     free(Lookup_Reverse);                                        //free allocated array in heap
     free(Window);                                                //free allocated array in heap
